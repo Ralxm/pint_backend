a94db5cb91a3366eb06914e87b3c6c852dd3067a
Ralxm <rafael61111@gmail.com>
2024-07-11 14:51:06 +0100

    mudancas

--- routes/colaborador_routes.js
+++ routes/colaborador_routes.js
@@ -8,7 +8,7 @@ router.post('/login', controller.colaboradorLogin);
 router.post('/create', controller.colaboradorCreate);
 router.get('/list', middleware.checkToken, controller.colaboradorList);
 router.get('/get/:id', middleware.checkToken, controller.colaboradorGet);
-router.put('/delete/:id', controller.colaboradorDelete);
+router.put('/delete/:id', middleware.checkToken, controller.colaboradorDelete);
 router.put('/update/:id', middleware.checkToken, controller.colaboradorUpdate);
 
 router.put('/getByEmail/:email', middleware.checkToken, controller.colaboradorGetByEmail);
