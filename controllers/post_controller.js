32b75397bf24c74b27db60569e1b20428f65eda2
Ralxm <rafael61111@gmail.com>
2024-09-11 11:47:34 +0100

    post data publicação

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -93,7 +93,7 @@ async function postList(req, res){
 controller.postListBlob = async (req, res) => {
     const { id } = req.params;
     const data = await Post.findAll({include: [Evento, Espaco, Categoria, Subcategoria, Aprovacao, Cidade, Colaborador]},
-        { where: { CIDADE: id}}, {order: [['DATAPUBLICACAO', 'ASC']]})
+        { where: { CIDADE: id}}, {order: [['DATAPUBLICACAO', 'DESC']]})
     .then(function(data) {
         res.status(200).json({
             success: true,
